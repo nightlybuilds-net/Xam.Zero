8d235939635a34edbbfc0f2313c219fb05c2869a
Gabriele Coppola <g.coppola.wp@gmail.com>
2019-02-02 11:02:46 +0100

    remove flyout from simple shell

--- Xam.Zero/Xam.Zero.SimpleTabbedApp/Xam.Zero.SimpleTabbedApp/ViewModels/LoginViewModel.cs
+++ Xam.Zero/Xam.Zero.SimpleTabbedApp/Xam.Zero.SimpleTabbedApp/ViewModels/LoginViewModel.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xam.Zero.Services;
 using Xam.Zero.SimpleTabbedApp.Pages;
@@ -10,14 +11,26 @@ namespace Xam.Zero.SimpleTabbedApp.ViewModels
     public class LoginViewModel: ZeroBaseModel
     {
         private readonly IShellService _shellService;
-        public ICommand GoToRegistrationCommand { get; private set; }
-        public ICommand LoginCommand { get; private set; }
+        public ICommand GoToRegistrationCommand { get; set; }
+        public ICommand LoginCommand { get; set; }
 
         public LoginViewModel(IShellService shellService)
         {
             _shellService = shellService;
-            this.GoToRegistrationCommand = new Command(async () => await base.GoModalTo<RegistrationPage>(null));
-            this.LoginCommand = new Command( () => _shellService.SwitchContainer<TabbedShell>());
+            
+            this.GoToRegistrationCommand = new Command(async () =>
+            {
+                await base.GoModalTo<RegistrationPage>(null);
+            });
+            this.LoginCommand = new Command( () =>
+            {
+                _shellService.SwitchContainer<TabbedShell>();
+            });
+        }
+
+        protected override async Task Init(object data)
+        {
+            await base.Init(data);
         }
     }
 }
