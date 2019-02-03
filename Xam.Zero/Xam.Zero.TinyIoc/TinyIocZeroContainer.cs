6e264042c047efb9502cf4d0d45d0ad2f6182891
Gabriele Coppola <g.coppola.wp@gmail.com>
2019-02-03 20:09:27 +0100

    [Wip] start defining tiny ioc implementation of Zero IoC

---
+++ Xam.Zero/Xam.Zero.TinyIoc/TinyIocZeroContainer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using IContainer = Xam.Zero.Ioc.IContainer;
+
+namespace Xam.Zero.TinyIoc
+{
+    public class TinyIocZeroContainer: IContainer
+    {
+        public TinyIocZeroContainer()
+        {
+        }
+        
+        public void Register<T>(bool transient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register<T, TImpl>(bool transient) where TImpl : T
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(Type type, bool transient)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RegisterInstance<T>(T instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
