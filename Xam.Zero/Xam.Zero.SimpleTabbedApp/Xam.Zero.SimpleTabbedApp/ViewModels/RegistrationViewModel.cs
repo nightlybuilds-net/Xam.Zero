53aa58fd6a68ab7f320275ee0b44fdb9aadcedd0
Gabriele Coppola <g.coppola.wp@gmail.com>
2019-02-04 21:47:42 +0100

    add display alert sample

--- Xam.Zero/Xam.Zero.SimpleTabbedApp/Xam.Zero.SimpleTabbedApp/ViewModels/RegistrationViewModel.cs
+++ Xam.Zero/Xam.Zero.SimpleTabbedApp/Xam.Zero.SimpleTabbedApp/ViewModels/RegistrationViewModel.cs
@@ -7,10 +7,15 @@ namespace Xam.Zero.SimpleTabbedApp.ViewModels
     public class RegistrationViewModel: ZeroBaseModel
     {
         public ICommand BackToLoginCommand { get; private set; }
+        public ICommand ClickMeCommand { get; private set; }
 
         public RegistrationViewModel()
         {
             this.BackToLoginCommand = new Command(async () => await base.PopModal());
+            this.ClickMeCommand = new Command(async () =>
+            {
+                await base.DisplayAlert("Info", "You clicked!", "OK");
+            });
         }
     }
 }
