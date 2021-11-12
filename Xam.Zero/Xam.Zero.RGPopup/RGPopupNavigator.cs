3e14f9acd9c7fcb6e794b8b401dd2c4c719eb535
CptPhenomeno <benvenutigab@gmail.com>
2021-11-12 17:14:48 +0100

    Add support to Rg.Plugin popups

---
+++ Xam.Zero/Xam.Zero.RGPopup/RGPopupNavigator.cs
@@ -0,0 +1,52 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+using Xam.Zero.Popups;
+using Xam.Zero.Services;
+using Xam.Zero.ViewModels;
+
+namespace Xam.Zero.RGPopup
+{
+    public class RGPopupNavigator : IPopupNavigator
+    {
+        public Type BasePopupType => typeof(PopupPage);
+
+        private readonly ObjectIDGenerator _objectIDGenerator;
+        private readonly Dictionary<long, object> _popupMapper;
+
+        private RGPopupNavigator()
+        {
+            this._objectIDGenerator = new ObjectIDGenerator();
+            this._popupMapper = new Dictionary<long,object>();
+        }
+
+        public static RGPopupNavigator Build()
+        {
+            return new RGPopupNavigator();
+        }
+
+        public async Task<T> ShowPopup<T>(IXamZeroPopup<T> popup)
+        {
+            var popupPage = (PopupPage)popup;
+            var completionSource = new TaskCompletionSource<T>();
+            var popupId = this._objectIDGenerator.GetId(popupPage, out var _);
+            this._popupMapper.Add(popupId, completionSource);
+            await PopupNavigation.Instance.PushAsync(popupPage);
+            var result = await completionSource.Task;
+            return result;
+        }
+
+        public Task DismissPopup<T>(IXamZeroPopup<T> popup, T result)
+        {
+            var popupPage = (PopupPage)popup;
+            var popupId = this._objectIDGenerator.GetId(popupPage, out var _);
+            var completionSource = (TaskCompletionSource<T>)this._popupMapper[popupId];
+            this._popupMapper.Remove(popupId);
+            completionSource.SetResult(result);
+            return PopupNavigation.Instance.RemovePageAsync(popupPage);
+        }
+    }
+}
